c7a9ffb7e76da6a5ca3aae4aa62e9c04f62b8a8d
juanromero3 <jromero2397@gmail.com>
2016-02-23 22:11:07 +0100

    kk

---
+++ juegoCoursera1/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  juegoCoursera1
+//
+//  Created by Juan Romero on 23/2/16.
+//  Copyright © 2016 Juan Romero. All rights reserved.
+//
+
+import UIKit
+
+var números = 0...100
+
+for núm in números { {
+    
+    print("núm")
+    }
+    
+    
+    if núm % 5 = 0{
+        
+        print("#número" + "Bingo!!!")
+        
+    }else if núm % 2 = 0{
+        
+        print("#número" + "par!!!")
+        
+    }else if num % 2 = 1{
+        
+        print("#número" + "impar!!!")
+        
+    }else if núm > 30 && núm < 40{
+        
+        print("#número" + "Viva Swift!!!")
+    }
+}
+
+
